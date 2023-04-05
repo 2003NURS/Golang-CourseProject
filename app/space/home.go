d458c54b9a16813d7c93ebfebc01474ae88eb0ae
Nursultan <nursamanzhol@gmail.com>
2023-04-05 13:41:19 +0600

    5

--- app/space/home.go
+++ app/space/home.go
@@ -1,31 +1,31 @@
 // "\app\space\home.go"
 package space
 
+import (
+	"fmt"
+	"net/http"
+	"errors"
+	"html/template"
+	"runtime/internal/sys"
+)
+
+var data = ""
+
 func Welcome(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Login running")
 	t, err := template.ParseFiles("templates/welcome.html")
- if err != nil {
-  http.Error(w, err.Error(), http.StatusInternalServerError)
-  return
- }
- err = t.Execute(w, data)
- if err != nil {
-  http.Error(w, err.Error(), http.StatusInternalServerError)
-  return
- }
-
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func HandlerRequest(){
-	
-
-	https.HandleFunc("/welcome/", Welcome)
-	https.ListenAndServe("localhost:8000", nil)
+	http.HandleFunc("/welcome/", Welcome)
+	http.ListenAndServe("localhost:8000", nil)
 }
-
-// 	t, err :=template.ParseFiles("templates/welcome.html")
-	
-//  if err != nil {
-//   fmt.Fprintf(w, err.Error())
-//  }
-//  t.Execute(w, "welcome")
