d455ad982b2b7ca2c8fc796992aef652637507fc
Nursultan <nursamanzhol@gmail.com>
2023-04-05 13:03:02 +0600

    1

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 // "\main.go"
 package main
 import (
-	"https://github.com/2003NURS/Golang-CourseProject/app/home"
+	"github.com/2003NURS/Golang-CourseProject/app/space"
 )
 
 func main(){
 	println("Hello")
+	println("http://localhost:8000/welcome")
+
+	home.HandlerRequest()
 }
