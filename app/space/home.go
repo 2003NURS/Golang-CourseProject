594e9033b261356d1d5d4ee5973bbeef88660339
Nursultan <nursamanzhol@gmail.com>
2023-04-05 13:30:32 +0600

    4

--- app/space/home.go
+++ app/space/home.go
@@ -1,18 +1,31 @@
 // "\app\space\home.go"
 package space
+
 func Welcome(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Login running")
-	t, err :=template.ParseFiles("templates/welcome.html")
-	
+	t, err := template.ParseFiles("templates/welcome.html")
  if err != nil {
-  fmt.Fprintf(w, err.Error())
+  http.Error(w, err.Error(), http.StatusInternalServerError)
+  return
  }
- t.Execute(w, "welcome")
+ err = t.Execute(w, data)
+ if err != nil {
+  http.Error(w, err.Error(), http.StatusInternalServerError)
+  return
+ }
+
 }
 
-function HandlerRequest(){
+func HandlerRequest(){
 	
 
 	https.HandleFunc("/welcome/", Welcome)
 	https.ListenAndServe("localhost:8000", nil)
 }
+
+// 	t, err :=template.ParseFiles("templates/welcome.html")
+	
+//  if err != nil {
+//   fmt.Fprintf(w, err.Error())
+//  }
+//  t.Execute(w, "welcome")
